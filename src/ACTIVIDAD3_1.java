ee0d3f5e175196eb7db3e9f6f440a236cb9e9b1a
AngelsGV <angels.not99@gmail.com>
2023-10-30 21:43:01 +0100

    Initial commit

--- src/ACTIVIDAD3_1.java
+++ src/ACTIVIDAD3_1.java
@@ -2,16 +2,27 @@ import java.util.Scanner;
 public class ACTIVIDAD3_1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int Emax, Emin;
-        System.out.println("Intrduzca una edad: ");
-        int num = sc.nextInt();
-        while ( num !<= -1){
-
-            System.out.println("La edad máxima es: " + Emax);
-            System.out.println("La edad minima es: " + Emin);
-            System.out.println("Intrduzca otra edad: ");
-            num = sc.nextInt();
-
+        int edad;
+        int eMaxima = Integer.MIN_VALUE; // Inicializamos con el valor mínimo posible
+        int eMinima = Integer.MAX_VALUE; // Inicializamos con el valor máximo posible
+        System.out.println("Ingrese las edades y al acabar de introducirlas inserte -1: ");
+        while (true) {
+            edad = sc.nextInt();
+            if (edad == -1) {
+                break;
+            }
+            if (edad > eMaxima) {
+                eMaxima = edad;
+            }
+            if (edad < eMinima) {
+                eMinima = edad;
+            }
+        }
+        if (eMaxima == Integer.MIN_VALUE && eMinima == Integer.MAX_VALUE) {
+            System.out.println("No se ingresaron edades.");
+        } else {
+            System.out.println("La edad máxima es: " + eMaxima);
+            System.out.println("La edad mínima es: " + eMinima);
         }
     }
 }
